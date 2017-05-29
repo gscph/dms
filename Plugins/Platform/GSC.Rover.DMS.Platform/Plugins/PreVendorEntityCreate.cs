4ab4515a0b2bf653a2bb696407bed8ef41b2e985
Rex Ghadaffi <jcadiao@gurango.net>
2017-05-29 15:31:17 +0800

    Initial Repository Deployment
    Platform and CRM SDK Tools for our deployer

---
+++ Plugins/Platform/GSC.Rover.DMS.Platform/Plugins/PreVendorEntityCreate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ServiceModel;
+using Microsoft.Xrm.Sdk;
+using GSC.Rover.DMS.BusinessLogic;
+namespace GSC.Rover.DMS.Platform.Plugins
+{
+    class PreVendorEntityCreate
+    {
+
+
+
+    }
+}
