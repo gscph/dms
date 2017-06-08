4571fddeb92fb8f74b5d2a52d731c26cb398d5b7
lesliebaliguat <baliguatleslie@gmail.com>
2017-06-08 15:14:25 +0800

    Fixed bug in validator

--- Javascript/edit-vehicleanditemcatalog-form.js
+++ Javascript/edit-vehicleanditemcatalog-form.js
@@ -30,50 +30,46 @@ $(document).ready(function () {
         return;
     }
 
-    createValidators();
+    var expiryDaysValidator = document.createElement("span");
+    expiryDaysValidator.style.display = "none";
+    expiryDaysValidator.id = "RequiredFieldValidatorgsc_warrantyexpirydays";
+    expiryDaysValidator.controltovalidate = "gsc_warrantyexpirydays";
+    expiryDaysValidator.errormessage = "<a href='#gsc_warrantyexpirydays'>Warranty expiry days must be numeric.</a>";
+    expiryDaysValidator.validationGroup = "";
+    expiryDaysValidator.initialvalue = "";
+    expiryDaysValidator.evaluationfunction = function () {
+        var value = $("#gsc_warrantyexpirydays").val();
+        var regex = /^[\d]*$/;
+
+        var matches = value.match(regex);
+
+        if (matches !== null) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+
+    var warrantyMileageValidator = document.createElement("span");
+    warrantyMileageValidator.style.display = "none";
+    warrantyMileageValidator.id = "RequiredFieldValidatorgsc_warrantymileage";
+    warrantyMileageValidator.controltovalidate = "gsc_warrantymileage";
+    warrantyMileageValidator.errormessage = "<a href='#gsc_warrantymileage'>Warranty mileage must be numeric.</a>";
+    warrantyMileageValidator.validationGroup = "";
+    warrantyMileageValidator.initialvalue = "";
+    warrantyMileageValidator.evaluationfunction = function () {
+        var value = $("#gsc_warrantymileage").val();
+        var regex = /^[\d]*$/;
+        var matches = value.match(regex);
+
+        if (matches !== null) {
+            return true;
+        }
+        else
+            return false;
+    };
 
     Page_Validators.push(expiryDaysValidator);
     Page_Validators.push(warrantyMileageValidator);
-
-    function createValidators() {
-        var expiryDaysValidator = document.createElement("span");
-        expiryDaysValidator.style.display = "none";
-        expiryDaysValidator.id = "RequiredFieldValidatorgsc_warrantyexpirydays";
-        expiryDaysValidator.controltovalidate = "gsc_warrantyexpirydays";
-        expiryDaysValidator.errormessage = "<a href='#gsc_warrantyexpirydays'>Warranty expiry days must be numeric.</a>";
-        expiryDaysValidator.validationGroup = "";
-        expiryDaysValidator.initialvalue = "";
-        expiryDaysValidator.evaluationfunction = function () {
-            var value = $("#gsc_warrantyexpirydays").val();
-            var regex = /^[\d]*$/;
-
-            var matches = value.match(regex);
-
-            if (matches !== null) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        };
-
-        var warrantyMileageValidator = document.createElement("span");
-        warrantyMileageValidator.style.display = "none";
-        warrantyMileageValidator.id = "RequiredFieldValidatorgsc_warrantymileage";
-        warrantyMileageValidator.controltovalidate = "gsc_warrantymileage";
-        warrantyMileageValidator.errormessage = "<a href='#gsc_warrantymileage'>Warranty mileage must be numeric.</a>";
-        warrantyMileageValidator.validationGroup = "";
-        warrantyMileageValidator.initialvalue = "";
-        warrantyMileageValidator.evaluationfunction = function () {
-            var value = $("#gsc_warrantymileage").val();
-            var regex = /^[\d]*$/;
-            var matches = value.match(regex);
-
-            if (matches !== null) {
-                return true;
-            }
-            else
-                return false;
-        };
-    }
 });
