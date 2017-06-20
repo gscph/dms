b115084667136992ce44f056018720c4ab9d39e7
raptoron <raphael.herrera7@gmail.com>
2017-06-20 18:04:34 +0800

    Code refactor

--- Javascript/new-bin-form.js
+++ Javascript/new-bin-form.js
@@ -1,30 +1,30 @@
 $(document).ready(function () {
-	$('#gsc_sitename').attr('readOnly', 'readOnly');
+	$("#gsc_sitename").attr("readOnly", "readOnly");
 
-	setTimeout(function autoPopulate(){
+	setTimeout(function(){
 
-    $('#gsc_siteid').on('change', function () {
-      var siteId = $('#gsc_siteid').val();
+    $("#gsc_siteid").on("change", function () {
+      var siteId = $("#gsc_siteid").val();
       var siteQuery = "/_odata/gsc_iv_site?$filter=gsc_iv_siteid eq (Guid'" + siteId + "')";
       
       $.ajax({
-        type: 'get',
+        type: "get",
         async: true,
         url: siteQuery,
         success: function (data) {
-          if (data.value.length != 0) {
+          if (data.value.length !== 0) {
             var site = data.value[0];
             var siteName = site.gsc_sitename;
             
-            $('#gsc_sitename').val(siteName);
+            $("#gsc_sitename").val(siteName);
           }
         },
         error: function (xhr, textStatus, errorMessage) {
-          $('#gsc_sitename').val('');
+          $("#gsc_sitename").val("");
           console.log(errorMessage);
         }
      });
- 	});
+  });
 	}, 1000);
 });
 
