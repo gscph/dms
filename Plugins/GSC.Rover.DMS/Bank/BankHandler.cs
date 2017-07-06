9363838f3d1f88804c7d5e3d923e94ccd778a9ba
raptoron <raphael.herrera7@gmail.com>
2017-07-06 09:42:57 +0800

    Initialize Bank Handler

---
+++ Plugins/GSC.Rover.DMS/Bank/BankHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GSC.Rover.DMS.BusinessLogic.City;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace GSC.Rover.DMS.BusinessLogic.Bank
+{
+    public class BankHandler
+    {
+         private readonly IOrganizationService _organizationService;
+        private readonly ITracingService _tracingService;
+
+        public BankHandler(IOrganizationService service, ITracingService trace)
+        {
+            _organizationService = service;
+            _tracingService = trace;
+        }
+
+        public Entity SetCity(Entity accountEntity)
+        {
+            CityHandler cityHandler = new CityHandler(_organizationService, _tracingService);
+            return cityHandler.SetCity(accountEntity);
+        }
+    }
+}
