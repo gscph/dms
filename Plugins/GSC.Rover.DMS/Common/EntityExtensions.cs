0dbe35478f9356159d27e3e1e557884acb43189c
Rex Ghadaffi <jcadiao@gurango.net>
2017-08-03 15:11:41 +0800

    Common Extensions Update

---
+++ Plugins/GSC.Rover.DMS/Common/EntityExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSC.Rover.DMS.BusinessLogic.Common
+{
+    public static class EntityExtensions
+    {
+        /// <summary>
+        /// Checks if entity attribute exists gets its value of an entity. returns default value if attribute does not exist
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="service"></param>
+        /// <returns></returns>
+        public static Guid GetEntityReferenceGuid(this Entity entity, string attribute)
+        {
+            if (entity.Attributes.Contains(attribute))
+            {
+                return entity.GetAttributeValue<EntityReference>(attribute).Id;
+            }
+
+            return default(Guid);
+        }
+    }
+}
