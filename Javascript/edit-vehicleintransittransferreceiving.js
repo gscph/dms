5992fb6e036436ccd135c3c8d730091b92be1e97
Rex Ghadaffi <jcadiao@gurango.net>
2017-08-14 12:08:33 +0800

    Intransit Receiving Javascript Update

--- Javascript/edit-vehicleintransittransferreceiving.js
+++ Javascript/edit-vehicleintransittransferreceiving.js
@@ -1,74 +1,75 @@
 //Created By : Raphael Herrera, Created On : 9/02/16
 $(document).ready(function () {
-  var intransitStatus = $('#gsc_intransitstatus').val();
 
+  var intransitStatus = $('#gsc_intransitstatus').val();
 
   if (intransitStatus == 100000000) {
     drawCancelButton();
     drawReceiveButton();
   }
-  drawPrintButton();
 
+  drawPrintButton();
+  drawComponentsButton();
+  
   $('#gsc_intransitstatus').attr('readOnly', true);
   $('#gsc_intransitstatus').css({ "pointer-events": "none", "cursor": "default" });
-
+  
   //Initialize editable grid
-  $(document).trigger('initializeEditableGrid', vehicleComponentChecklistGridInstance);
+//  $(document).trigger('initializeEditableGrid', vehicleComponentChecklistGridInstance);
 
-   setTimeout(function(){
-    if(intransitStatus == 100000001 || intransitStatus == 100000002) //Received || Cancelled
-      {
-        $('form fieldset').attr('disabled', true);
-        $(".datetimepicker input").attr('disabled', true);
-        if(intransitStatus == 100000001)
-        {
-          $('*[data-name="VehicleComponentChecklist"]').parent().removeAttr("disabled");
-        }
+  setTimeout(function () {
+    if (intransitStatus == 100000001 || intransitStatus == 100000002) //Received || Cancelled
+    {
+      $('form fieldset').attr('disabled', true);
+      $(".datetimepicker input").attr('disabled', true);
+      if (intransitStatus == 100000001) {
+        $('*[data-name="VehicleComponentChecklist"]').parent().removeAttr("disabled");
       }
-
-      if(intransitStatus == 100000000)
-         $('*[data-name="VehicleComponentChecklist"]').parent().attr("disabled", "disabled");
-   },3000);
-});
-
-var vehicleComponentChecklistGridInstance = {
-  initialize: function () {
-    /* - Editable Grid - Vehicle Component Checklist Subgrid*/
-    $('<div id="vehiclecomponentchecklist-editablegrid" class="editable-grid"></div>').appendTo('.content-wrapper');
-    var $container = document.getElementById('vehiclecomponentchecklist-editablegrid');
-    var idQueryString = DMS.Helpers.GetUrlQueryString('id');
-    var oDataQuery = '/_odata/gsc_iv_vehicleintransitreceivingchecklist?$filter=gsc_intransitreceivingid/Id%20eq%20(Guid%27' + idQueryString + '%27)';
-    var screenSize = ($(window).width() / 2) - 80;
-    var options = {
-      dataSchema: {
-        gsc_iv_vehicleintransitreceivingchecklistid: { Id: null, Name: null }, 
-        gsc_included: null,
-        gsc_vehicleintransitreceivingchecklistpn: ''
-      },
-      colHeaders: [
-        'Included', 'Vehicle Checklist'
-      ],
-      columns: [
-        { data: 'gsc_included', type: 'checkbox', renderer: checkboxRenderer, className: 'htCenter htMiddle', width: 80 },
-        { data: 'gsc_vehicleintransitreceivingchecklistpn', renderer: stringRenderer, readOnly: true, className: 'htCenter htMiddle', width: 200 }
-      ],
-      gridWidth: 1000,
-      addNewRows: false,
-      deleteRows: false
     }
 
-    var sectionName = 'VehicleComponentChecklist';
-    var attributes = [{ key: 'gsc_included', type: 'System.Boolean' }, 
-    { key: 'gsc_vehicleintransitreceivingchecklistpn', type: 'System.String' },
-    { key: 'gsc_intransitreceivingid', type: 'Microsoft.Xrm.Sdk.EntityReference', reference: 'gsc_iv_vehicleintransittransferreceiving', value: idQueryString }];
-    var model = { id: 'gsc_iv_vehicleintransitreceivingchecklistid', entity: 'gsc_iv_vehicleintransitreceivingchecklist', attr: attributes };
-    var hotInstance = EditableGrid(options, $container, sectionName, oDataQuery, model, {
-            gsc_iv_vehicleintransitreceivingchecklistid: null,
-            gsc_included: false,
-            gsc_vehicleintransitreceivingchecklistpn: ''
-        });
-  }
-}
+    if (intransitStatus == 100000000)
+      $('*[data-name="VehicleComponentChecklist"]').parent().attr("disabled", "disabled");
+  }, 3000);
+});
+
+// var vehicleComponentChecklistGridInstance = {
+//   initialize: function () {
+//     /* - Editable Grid - Vehicle Component Checklist Subgrid*/
+//     $('<div id="vehiclecomponentchecklist-editablegrid" class="editable-grid"></div>').appendTo('.content-wrapper');
+//     var $container = document.getElementById('vehiclecomponentchecklist-editablegrid');
+//     var idQueryString = DMS.Helpers.GetUrlQueryString('id');
+//     var oDataQuery = '/_odata/gsc_iv_vehicleintransitreceivingchecklist?$filter=gsc_intransitreceivingid/Id%20eq%20(Guid%27' + idQueryString + '%27)';
+//     var screenSize = ($(window).width() / 2) - 80;
+//     var options = {
+//       dataSchema: {
+//         gsc_iv_vehicleintransitreceivingchecklistid: { Id: null, Name: null },
+//         gsc_included: null,
+//         gsc_vehicleintransitreceivingchecklistpn: ''
+//       },
+//       colHeaders: [
+//         'Included', 'Vehicle Checklist'
+//       ],
+//       columns: [
+//         { data: 'gsc_included', type: 'checkbox', renderer: checkboxRenderer, className: 'htCenter htMiddle', width: 80 },
+//         { data: 'gsc_vehicleintransitreceivingchecklistpn', renderer: stringRenderer, readOnly: true, className: 'htCenter htMiddle', width: 200 }
+//       ],
+//       gridWidth: 1000,
+//       addNewRows: false,
+//       deleteRows: false
+//     }
+
+//     var sectionName = 'VehicleComponentChecklist';
+//     var attributes = [{ key: 'gsc_included', type: 'System.Boolean' },
+//     { key: 'gsc_vehicleintransitreceivingchecklistpn', type: 'System.String' },
+//     { key: 'gsc_intransitreceivingid', type: 'Microsoft.Xrm.Sdk.EntityReference', reference: 'gsc_iv_vehicleintransittransferreceiving', value: idQueryString }];
+//     var model = { id: 'gsc_iv_vehicleintransitreceivingchecklistid', entity: 'gsc_iv_vehicleintransitreceivingchecklist', attr: attributes };
+//     var hotInstance = EditableGrid(options, $container, sectionName, oDataQuery, model, {
+//       gsc_iv_vehicleintransitreceivingchecklistid: null,
+//       gsc_included: false,
+//       gsc_vehicleintransitreceivingchecklistpn: ''
+//     });
+//   }
+// }
 
 function preventDefault(event) {
   event.preventDefault();
@@ -119,5 +120,19 @@ function printInTransitReceiving() {
   var host = window.location.host;
   var url = protocol + "//" + host + "/report/?reportname={82439C4C-4973-E611-80DC-00155D010E2C}&reportid=" + param1var;
   window.open(url, 'blank', 'width=500,height=400');
+}
 
+function drawComponentsButton() {
+  $container = $('<div class="view-toolbar grid-actions clearfix"></div>');
+  $buttonIcon = DMS.Helpers.CreateFontAwesomeIcon('fa-wrench');
+
+  $button = DMS.Helpers.CreateButton('button', true, 'components', ' COMPONENTS', $buttonIcon);
+
+  $button.click(function () {
+    alert('hey');
+  });
+
+  $container.append($button);
+
+  $('#VehicleInTransitTransferReceivingDetail .subgrid').prepend($container);
 }
